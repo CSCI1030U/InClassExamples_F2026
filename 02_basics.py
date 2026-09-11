f49823832d2e30ac5ecd0678e7387435b6496a66
randyfortier <randyfortier@users.noreply.github.com>
2026-09-11 11:31:35 -0400

    Added some comments to explain the code for posterity

--- 02_basics.py
+++ 02_basics.py
@@ -1,7 +1,5 @@
 # input and output
 
-# commented because of testing slog
-
 # name: str = input('What is your name? ')
 # age_string: str = input('How old are you? ')
 # age: int = int(age_string)
@@ -9,22 +7,23 @@
 # str(18)
 # float('3.14159')
 # float(18)
+# above is commented out since testing code with input() is tedious
 name: str ='Kumar'
 age: int = 9
 
-'Hello\nGoodbye'
+# an awkward way to create a multi-line string
+message1: str = 'Hello\nGoodbye'
 
-'''
+# multi-line strings
+message2: str = '''
 Line 1
 Another line
 Again...
 '''
 
-
+# format strings - let you add dynamic data (variables and computed values) to a string
 message: str = f'In 5 years, {name} will be {age + 5} years old.'
 print(message)
 
-
+# all in one line
 print(f'In 5 years, {name} will be {age + 5} years old.')
-
-# other code to test
