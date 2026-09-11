cf8464cbcca5f428833be50d13af5134a625044c
randyfortier <randyfortier@users.noreply.github.com>
2026-09-11 11:15:57 -0400

    Added first basic programming code

---
+++ 02_basics.py
@@ -0,0 +1,30 @@
+# input and output
+
+# commented because of testing slog
+
+# name: str = input('What is your name? ')
+# age_string: str = input('How old are you? ')
+# age: int = int(age_string)
+# age = int(input('How old are you? '))
+# str(18)
+# float('3.14159')
+# float(18)
+name: str ='Kumar'
+age: int = 9
+
+'Hello\nGoodbye'
+
+'''
+Line 1
+Another line
+Again...
+'''
+
+
+message: str = f'In 5 years, {name} will be {age + 5} years old.'
+print(message)
+
+
+print(f'In 5 years, {name} will be {age + 5} years old.')
+
+# other code to test
